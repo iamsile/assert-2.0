7ef2c836d343da39c03b2be653e99b8919043f8d
sile <iamsai007@sina.com>
2014-02-08 22:39:00 +0800

    Initial Commit

---
+++ assert 2.0/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  assert 2.0
+//
+//  Created by taowei on 14-2-8.
+//  Copyright (c) 2014年 taowei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
